43dc8a0388d1655e550b24be338d18b27477e519
radmanplays <95340057+radmanplays@users.noreply.github.com>
2023-09-04 13:33:38 +0330

    Update fly.js

--- fly.js
+++ fly.js
@@ -1,5 +1,5 @@
 PluginAPI.require("player");
-PluginAPI.addEventListener("key", (h)=>{ //Every client tick
+PluginAPI.addEventListener("update", () => {
   PluginAPI.player.motionY = 0.9; //Default ladder speed
   PluginAPI.updateComponent("player"); //Tell api that player needs updating
   }
