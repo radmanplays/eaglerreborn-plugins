cda3b4fd8716f095bfaa172b19c39bb8bb73065f
radmanplays <95340057+radmanplays@users.noreply.github.com>
2023-09-04 15:00:38 +0330

    Update jetpack.js

--- jetpack.js
+++ jetpack.js
@@ -2,7 +2,7 @@ PluginAPI.require("player");
 
 PluginAPI.addEventListener("key", (event) => {
   if (event.key === 35) { // Key code for 'h'
-    PluginAPI.player.motionY += 5;
+    PluginAPI.player.motionY += 0.5;
     PluginAPI.updateComponent("player");
   }
 });
