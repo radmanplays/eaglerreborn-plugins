d1840ea095c3f36ba6396370983b0d8966f30fda
radmanplays <95340057+radmanplays@users.noreply.github.com>
2023-09-05 16:46:05 +0330

    Update message-cloaks.js

--- message-cloaks.js
+++ message-cloaks.js
@@ -3,7 +3,7 @@ PluginAPI.addEventListener("sendchatmessage", function (event) {
   var message = event.message;
 
   // Modify the chat message
-  var modifiedMessage = "♦ " + message;
+  var modifiedMessage = " >> " + message;
 
   // Prevent the default action (cancels sending the packet) if needed
   // event.preventDefault();
