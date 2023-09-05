7b795106156282c580daf0d6a9c0b2e11feb4301
radmanplays <95340057+radmanplays@users.noreply.github.com>
2023-09-05 16:27:39 +0330

    Update and rename chat.js to message-cloaks.js

--- message-cloaks.js
+++ message-cloaks.js
@@ -3,7 +3,7 @@ PluginAPI.addEventListener("sendchatmessage", function (event) {
   var message = event.message;
 
   // Modify the chat message
-  var modifiedMessage = "hhh " + message + " hhh";
+  var modifiedMessage = "🔹 " + message;
 
   // Prevent the default action (cancels sending the packet) if needed
   // event.preventDefault();
