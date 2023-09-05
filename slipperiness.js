ed128ca0de973cd1a76e5085d55efce9d7129f8a
radmanplays <95340057+radmanplays@users.noreply.github.com>
2023-09-05 13:40:56 +0330

    Update slipperiness.js

--- slipperiness.js
+++ slipperiness.js
@@ -1,7 +1,7 @@
 var blockKeys = Object.keys(PluginAPI.blocks);
 blockKeys.forEach(key=>{
    if(PluginAPI?.blocks?.[key]?.slipperiness) {// TeaVM likes to add metadata properties which are `null` or `undefined`
-      PluginAPI.blocks[key].slipperiness = 0.98; //Ice slipperiness value.
+      PluginAPI.blocks[key].slipperiness = 5; //Ice slipperiness value.
       PluginAPI.blocks[key].reload(); //The new method, `PluginAPI.updateComponent` is obsolete now.
    }
 });
