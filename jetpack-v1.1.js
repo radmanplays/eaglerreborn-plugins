b4a1464f677d6d236d0211e3563fa42d61bbcd53
radmanplays <95340057+radmanplays@users.noreply.github.com>
2023-09-09 13:01:48 +0330

    Update jetpack-v1.1.js

--- jetpack-v1.1.js
+++ jetpack-v1.1.js
@@ -1,7 +1,7 @@
 PluginAPI.require("player");
 
-PluginAPI.addEventListener("keydown", (event) => {
-  if (event.keyCode === 35) { // Key code for 'h'
+window.addEventListener("keydown", (event) => {
+  if (event.key === 35) { // Key code for 'h'
     PluginAPI.player.motionY += 0.2;
     PluginAPI.updateComponent("player");
   }
