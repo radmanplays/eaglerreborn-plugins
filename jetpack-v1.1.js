9dc270b5c22d5587c1b0a42f7fe4cff57430aab8
radmanplays <95340057+radmanplays@users.noreply.github.com>
2023-09-10 18:22:12 +0330

    Update jetpack-v1.1.js

--- jetpack-v1.1.js
+++ jetpack-v1.1.js
@@ -3,19 +3,19 @@ var jetpackActive = false;
 window.addEventListener("keydown", (event) => {
   if (event.key.toLowerCase() === "h") {
     jetpackActive = true;
+    PluginAPI.player.speedInAir = 0.2;
   }
 });
 window.addEventListener("keyup", (event) => {
   if (event.key.toLowerCase() === "h") {
     jetpackActive = false;
+    PluginAPI.player.speedInAir = 0;
   }
 });
 PluginAPI.addEventListener("update", ()=>{
 if(!jetpackActive){
-PluginAPI.player.speedInAir = 0;
 return;
 }
 PluginAPI.player.motionY += 0.2;
 PluginAPI.updateComponent("player");
-PluginAPI.player.speedInAir = 0.2;
 });
