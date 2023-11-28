44a8a83768188c8b87c81fd9e5e928019ba37987
radmanplays <95340057+radmanplays@users.noreply.github.com>
2023-11-28 19:42:23 +0330

    Update packetlogger-v1.0.js

--- packetlogger-v1.0.js
+++ packetlogger-v1.0.js
@@ -1,8 +1,10 @@
-// this plugin logs you packets and stores it in a txt file that you can download by doing .downloadpacket
+// this plugin logs you packets and stores it in a txt file that you can download by doing .downloadpackets
+var date = new Date()
+var properties
 PluginAPI.addEventListener("event", (ev) => {
-  if (
-    ev.event.startsWith("sendpacket")
-  ) {
-    console.log("[packetlogger] "+ev.event)
+  if (ev.event.startsWith("sendpacket")) {
+    date = new Date()
+    properties = ev.data
+    console.log("[packetsent]["+ date +"] "+ev.event+ " with properties: " + properties)
   }
 });
